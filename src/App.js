19bec7fbd0962bb6a19b75f8511d6ece9c208411
devdevil-production <saudd2gh@gmail.com>
2020-04-28 20:20:59 +0400

    Get movies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import queryString from "querystring";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      const queryData = {
+        api_key: process.env.REACT_APP_API_KEY,
+        query: "harry",
+        language: "en-US",
+        page: 2,
+      };
+      console.log(queryString.stringify(queryData));
+      const res = await fetch(
+        `${
+          process.env.REACT_APP_API_ENDPOINT
+        }/search/movie?${queryString.stringify(queryData)}`,
+        {
+          method: "GET",
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
