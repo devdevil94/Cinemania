58a63f6203b6d6733061dfac39e23d5398f28ba6
devdevil-production <saudd2gh@gmail.com>
2020-05-02 12:31:11 +0400

    Minor changes

--- src/App.js
+++ src/App.js
@@ -40,13 +40,13 @@ function App() {
             ({ title, poster_path }, i) =>
               poster_path && (
                 <div key={i} className="col ">
-                  <div className="card">
+                  <div className="card h-100">
                     <img
                       src={`${process.env.REACT_APP_IMAGE_BASE_URL}/w342${poster_path}`}
                       className="card-img-top"
                       alt={title}
                     />
-                    <div className="card-body">
+                    <div className="card-body d-flex justify-content-between flex-column">
                       <h5 className="card-title">{title}</h5>
                       <a href="#!" className="btn btn-primary">
                         View Details
